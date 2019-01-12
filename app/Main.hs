da505dccdace427ae73a85acfe9fa4ca02f95c1b
willians oliveira <willianszwy@gmail.com>
2019-01-12 04:46:03 -0400

    package remove

--- app/Main.hs
+++ app/Main.hs
@@ -1,23 +1,4 @@
 module Main where
 
-import           UI.NCurses
 main :: IO ()
-main = runCurses $ do
-    setEcho False
-    w <- defaultWindow
-    updateWindow w $ do
-        moveCursor 1 10
-        drawString "JUST ANOTHER NES EMULATOR"
-        moveCursor 3 10
-        drawString "(press q to quit)"
-        moveCursor 0 0
-    render
-    waitFor w (\ev -> ev == EventCharacter 'q' || ev == EventCharacter 'Q')
-
-waitFor :: Window -> (Event -> Bool) -> Curses ()
-waitFor w p = loop  where
-    loop = do
-        ev <- getEvent w Nothing
-        case ev of
-            Nothing  -> loop
-            Just ev' -> if p ev' then return () else loop
+main = putStrLn "Just Another Nes Emulator"
